038303af5b1509a15667dcfbac48f12c98ef4312
Christopher Marcum <zero6767@gmail.com>
2022-04-23 20:39:11 -0500

    Updated duplication removal, custom search string data, added batch file

---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set temp=False
+set multi=True
+set dupe=True
+set date=False
+set lang=False
+set verbose=2
+
+python google_search_scraper.py %temp% %multi% %dupe% %date% %lang% %verbose%
